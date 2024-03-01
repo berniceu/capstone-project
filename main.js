52368bd61df67631f24734169bc345378ab83735
berniceu <berniceuwituze@gmail.com>
2024-03-01 16:48:37 +0200

    add javascript to hamburger menu

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ const hamburgerButton = document.querySelector('.hamburger-btn');
 const navLinks = document.querySelector(".nav-links");
 
 hamburgerButton.addEventListener('click', () => {
-    navLinks.style.display = navLinks.style.display == 'none' ? 'block' : 'none';
+    if (navLinks.style.display === 'none' || navLinks.style.display === '') {
+        navLinks.style.display = 'block';
+        hamburgerButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    } else {
+        navLinks.style.display = 'none';
+        hamburgerButton.innerHTML = '☰';
+    }
 })
+
