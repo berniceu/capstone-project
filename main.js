39185a3ff4192b1f78be041c979e451e0ce42425
berniceu <berniceuwituze@gmail.com>
2024-03-01 18:09:22 +0200

    add comments in main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const hamburgerButton = document.querySelector('.hamburger-btn');
+const hamburgerBtn = document.querySelector('.hamburger-btn');
 const navLinks = document.querySelector(".nav-links");
 
-hamburgerButton.addEventListener('click', () => {
+
+// target hamburger button to display navlinks when clicked and turn the button into x-mark
+hamburgerBtn.addEventListener('click', () => {
     if (navLinks.style.display === 'none' || navLinks.style.display === '') {
         navLinks.style.display = 'block';
-        hamburgerButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+        hamburgerBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     } else {
         navLinks.style.display = 'none';
-        hamburgerButton.innerHTML = '☰';
+        hamburgerBtn.innerHTML = '☰';
     }
 })
 
