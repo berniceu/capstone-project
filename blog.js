16ecc76722f720036a637046e36581509c8e55f6
bernice uwituze <b.uwituze@alustudent.com>
2024-04-15 12:04:08 +0200

    add update function

--- blog.js
+++ blog.js
@@ -1,27 +1,27 @@
-const publishBtn = document.getElementById('publish');
+const publishBtn = document.getElementById("publish");
 const article = document.getElementById("story");
 const articleTitle = document.getElementById("title");
-const newArticleContainer = document.querySelector('.new-article-container');
+const newArticleContainer = document.querySelector(".new-article-container");
 
 //display write new post container
 
-const newPostBtn = document.getElementById('new-post-button');
-const hiddenPost = document.querySelector('.create-article.hidden');
+const newPostBtn = document.getElementById("new-post-button");
+const hiddenPost = document.querySelector(".create-article.hidden");
 
-if (newPostBtn){
-    newPostBtn.addEventListener('click', () => {
-        if (hiddenPost.style.display == 'none'){
-            hiddenPost.style.display = 'block';
-        } else {
-            hiddenPost.style.display = 'none';
-        }
-    });
+if (newPostBtn) {
+  newPostBtn.addEventListener("click", () => {
+    if (hiddenPost.style.display == "none") {
+      hiddenPost.style.display = "block";
+    } else {
+      hiddenPost.style.display = "none";
+    }
+  });
 }
-
 
 const blogForm = document.querySelector(".new-story-form");
 blogForm.addEventListener("submit", async function (e) {
-  e.preventDefault();
+  if (publishBtn.textContent == 'Publish'){
+    e.preventDefault();
 
   const title = document.getElementById("title");
   const story = document.getElementById("story");
@@ -42,12 +42,15 @@ blogForm.addEventListener("submit", async function (e) {
 
     if (res.ok) {
       alert("blog created successfully");
+      location.reload()
     } else {
       console.log("blog not created");
     }
   } catch (err) {
     console.log(err);
   }
+  }
+  
 });
 
 //retrieve and display
@@ -69,7 +72,7 @@ async function displayBlogs() {
         const blogsContainer = document.querySelector(".blogs-container");
         const blogsDiv = document.createElement("div");
         blogsDiv.classList.add("blogs");
-        blogsDiv.dataset.id = blog._id
+        blogsDiv.dataset.id = blog._id;
         blogsContainer.appendChild(blogsDiv);
         const blogTitle = document.createElement("div");
         blogTitle.classList.add("blog-title");
@@ -94,76 +97,100 @@ async function displayBlogs() {
     );
 }
 
-displayBlogs();
+window.addEventListener('DOMContentLoaded', displayBlogs)
 
 //update blogs
 
+const blogsContainer = document.querySelector(".blogs-container");
 
-const editBtn = document.querySelector('edit-button');
+blogsContainer.addEventListener("click", async function (e) {
+  const editBtn = e.target.closest(".edit-button");
+  const deleteBtn = e.target.closest(".delete-button");
 
-if(editBtn){
-    editBtn.addEventListener('click', async function(){
-    
-        hiddenPost.style.display = 'block';
-        const title = document.getElementById("title");
-        const story = document.getElementById("story");
-        const blogDiv = document.querySelector('.blogs');
-        const blogId = blogDiv.dataset.id;
-    
-    
-        const blogData = {
-            blogTitle: title.value,
-            blog: story.value,
-        };
-    
-        try{
-            const res = await fetch(`http://localhost:5000/blogs/${blogId}`, {
-            method:'PUT',
-            headers:{'Content-Type': 'application/json'},
-            body: JSON.stringify(blogData),
-    
-        });
-    
-        if (res.ok){
-            alert('blog updated successfully');
-            displayBlogs();
-        } else {
-            alert('blog update failed')
-        }
-        } catch(err){
-            console.log(err)
-        }
-        
-    
-    
-    })
-}
-
-// delete
-
-
-document.querySelector('.blogs-container').addEventListener('click', async function(e){
-  const deleteBtn = e.target.closest('.delete-button');
-  if(deleteBtn){
-    const blogDiv = deleteBtn.closest('.blogs');
+  if (editBtn) {
+    const blogDiv = editBtn.closest(".blogs");
     const blogId = blogDiv.dataset.id;
 
-    try{
+    try {
+      const res = await fetch(`http://localhost:5000/blogs/${blogId}`)
+      if(!res.ok){
+        console.log('failed to fetch blog')
+      }
+      const blogData = await res.json();
+       
+      hiddenPost.style.display = "block";
+      publishBtn.textContent = 'update'
+      const title = document.getElementById("title");
+      const story = document.getElementById("story");
+
+      title.value = blogData.blogTitle;
+      story.value = blogData.blog;
+
+      if (publishBtn.textContent == 'update'){
+        publishBtn.addEventListener('click', async function(e){
+  
+          const title = document.getElementById('title');
+          const story = document.getElementById('story');
+          
+          try{
+            const updateRes = await fetch(`http://localhost:5000/blogs/${blogId} `, {
+              method: 'PUT',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({blogTitle: title.value, blog: story.value})
+            });
+        
+            if(updateRes.ok){
+              alert('blog updated successfully');
+              location.reload()
+              
+              hiddenPost.style.display = 'none';
+              
+            } else{
+              alert('blog update failed')
+            }
+            
+          } catch(err){
+            console.log(err)
+          }
+        })
+      }
+      
+
+      
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // delete blogs
+
+  if (deleteBtn) {
+    const blogDiv = deleteBtn.closest(".blogs");
+    const blogId = blogDiv.dataset.id;
+
+    try {
       const res = await fetch(`http://localhost:5000/blogs/${blogId}`, {
-          method: 'DELETE',
-          headers: {'Content-Type': 'application/json'}
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
       });
 
-      if(res.ok){
-          alert('deleted successfully')
+      if (res.ok) {
+        location.reload()
+        alert("deleted successfully");
+        
       } else {
-          alert('delete failed')
+        alert("delete failed");
       }
-  } catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
+    }
   }
-  }
-})
+});
+
+
+
+
+
 
 
 
